a23848c864eba182ef9e68a14f566e49441f151b
Rust <pjy9954@gmail.com>
2015-05-01 14:47:55 +0900

    chrome의 Rich notification 대신 webkit Notification 사용하도록 변경

--- js/background.js
+++ js/background.js
@@ -1,46 +1,67 @@
-function show_notification(message) {
+function show_notification_webkit(title, body, id) {
 	var opt = {
-		type: "basic",
-		title: "케이포트의 공유노기",
-		message: message,
-		iconUrl: "image/82_black.png"
+		body: body,
+		tag: id,
+		icon: "image/48_black.png"
 	}
-
-	chrome.notifications.create(opt);
+	var notification = new Notification(title, opt);
+	return notification;
 }
 
-function make_str(data) {
-	var str_tmp = "";
+function make_body_arrival(data) {
+	var str = "";
 
-	str_tmp = decodeURIComponent(data.name) + "님이 " + data.channel + "채널에 ";
+	str = data.channel + "채널에 ";
 
 	switch(data.boss) {
-		case "B": str_tmp+="블랙드래곤"; break;
-		case "W": str_tmp+="화이트드래곤"; break;
-		case "R": str_tmp+="레드드래곤"; break;
-		case "D": str_tmp+="사막드래곤"; break;
-		case "E": str_tmp+="트릭스터"; break;
-		default : str_tmp+="error"; break;
+		case "B": str+="블랙드래곤이 "; break;
+		case "W": str+="화이트드래곤이 "; break;
+		case "R": str+="레드드래곤이 "; break;
+		case "D": str+="사막드래곤이 "; break;
+		case "E": str+="트릭스터가 "; break;
+		default : return 1;
 	}
 
-	switch(data.state.split("_")[0]){
-		case "arrival": str_tmp+="의 출현을 "; break;
-		case "0": str_tmp+="의 죽음을 "; break;
-		case "now": str_tmp+="을 잡고있음을 "; break;
-		case "skip": str_tmp+="의 패스를 "; break;
-		default : str_tmp+="error"; break;
+	str+="출현했습니다";
+
+	return str;
+}
+
+function make_title(boss) {
+	var title;
+
+	switch(boss) {
+		case "B": title="블랙드래곤"; break;
+		case "W": title="화이트드래곤"; break;
+		case "R": title="레드드래곤"; break;
+		case "D": title="사막드래곤"; break;
+		case "E": title="트릭스터"; break;
+		default : return 1;
 	}
 
-	switch(data.state.split("_")[1]){
-		case "y": str_tmp+="제보"; break;
-		case "n": str_tmp+="취소"; break;
-		case "c": str_tmp+="초기화"; break;
-		default : str_tmp+="error"; break;
+	title += " 진행상황 알림";
+
+	return title;
+}
+
+
+function make_body_now(data) {
+	var str = "";
+
+	str = data.channel + "채널에서 ";
+
+	switch(data.boss) {
+		case "B": str+="블랙드래곤을 "; break;
+		case "W": str+="화이트드래곤을 "; break;
+		case "R": str+="레드드래곤을 "; break;
+		case "D": str+="사막드래곤을 "; break;
+		case "E": str+="트릭스터를 "; break;
+		default : return 1;
 	}
 
-	str_tmp+="하였습니다.";
+	str+="잡고있습니다";
 
-	return str_tmp;
+	return str;
 }
 
 var serverArray = new Array("L", "H", "W", "M");
@@ -48,6 +69,7 @@ var raidArray = new Array("B", "W", "D", "R");
 var server;
 var raids = [];
 var sockets = [];
+var nowNotificationsArray = [];
 
 chrome.storage.local.get({
 	server: 1,
@@ -57,7 +79,7 @@ chrome.storage.local.get({
 	raids = items.raids;
 
 	for (var i=0 ; i<raids.length ; i++) {
-		if (raids[i] == true) {
+		if (raids[i] === true) {
 			sockets[sockets.length] = io.connect("http://211.109.170.90:12345/socket_" + serverArray[server] + raidArray[i]);
 		}
 	}
@@ -65,15 +87,54 @@ chrome.storage.local.get({
 	for (var i=0 ; i<sockets.length ; i++) {
 		sockets[i].on("client", function(data){
 			if (data.state.split("_")[0] != "checker") {
-				var notify_str = make_str(data);
-				show_notification(notify_str);
+				var status = data.state.split("_")[0];
+				var timelimit;
+				var notifyBody;
+				var notifyTitle;
+				var notification;
+				var notificationId = data.server + data.boss + data.channel;
+				var sec = 1000;
+
+				notifyTitle = make_title(data.boss);			
+
+				switch(status){
+					case "arrival":
+					timelimit = 7*sec;
+					notifyBody = make_body_arrival(data);
+					notification = show_notification_webkit(notifyTitle, notifyBody, notificationId);
+					break;
+
+					case "now":
+					notifyBody = make_body_now(data);
+					notification = show_notification_webkit(notifyTitle, notifyBody, notificationId);
+					nowNotificationsArray.push(notification);
+					break;
+
+					case "0":
+					for (var i=0 ; i<nowNotificationsArray.length ; i++) {
+						if (nowNotificationsArray[i].tag === notificationId) {
+							nowNotificationsArray[i].close();
+							nowNotificationsArray.splice(i,1);
+						}
+					}
+					break;
+
+					default : 
+					return 1;
+				}
+
+				if (timelimit) {
+					setTimeout(function() {
+						notification.close();
+					}, timelimit);
+				}
 			}
 		});
 	}
 });
 
 chrome.runtime.onMessage.addListener(function(message) {
-	if (message == "refresh") {
+	if (message === "refresh") {
 		for (var i=0 ; i<sockets.length ; i++) {
 			sockets[i].close();
 		}
